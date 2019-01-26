89aaafa5b04d5524ab2b097c83fba78081af666b
mvmijares <mvmijares@users.noreply.github.com>
2019-01-25 20:36:01 -0500

    Organized projects and started working on scripts

---
+++ CookingWithTita/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    #region Data
+    [SerializeField] private Player player;
+    public Player GetPlayer() { return player; }
+    public int playerScore;
+
+    #endregion
+    private void Awake() {
+        player = FindObjectOfType<Player>();
+        if (player)
+            player.InitializePlayer(this);
+    }
+
+    private void Update() {
+        
+    }
+}
---
+++ CookingWithTita/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private GameManager _gameManager;
+    private PlayerController _playerController;
+    private PlayerInput _playerInput;
+	public void InitializePlayer(GameManager gameManager) {
+        _gameManager = gameManager;
+        _playerINput
+
+    }
+
+    private void UpdatePlayer() {
+
+    }
+}
